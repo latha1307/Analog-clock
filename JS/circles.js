c98bad4cb4365ae984e2d13a6b9a601ac08104c0
latha1307 <ldkannammal@gmail.com>
2024-03-08 21:55:31 +0530

    Modern Analog Clock is Ready

---
+++ JS/circles.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+
+    let hh = document.getElementById('hh');
+    let mm = document.getElementById('mm');
+    let ss = document.getElementById('ss');
+
+    hh.style.strokeDashoffset = 510 - (h*510)/12;
+    mm.style.strokeDashoffset = 630 - (m*630)/60;
+    ss.style.strokeDashoffset = 760 - (s*760)/60;
+})
---
+++ JS/niddles.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+   
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    let hh = document.getElementById('hr');
+    let mm = document.getElementById('mn');
+    let ss = document.getElementById('sc');
+
+    sc.style.transform = `rotate(${s * 6}deg)`; // 360/60 = 6
+    mn.style.transform = `rotate(${m * 6}deg)`; // 360/60 = 6
+    hr.style.transform = `rotate(${h * 30}deg)`; // 360/12 = 30
+})
